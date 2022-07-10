f0064046b47c466d19a5331c6dd54b2eac6b8bba
lubov <loveavduhova@gmail.com>
2022-07-10 23:14:11 +0700

    task 4 done

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 4: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным. */
+
+
+int Prompt (string message)
+{
+Console.Write (message);
+int a = int.Parse (Console.ReadLine ());
+return a;
+}
+
+
+int number = Prompt ("Введите число ");
+
+if (number >= 1 && number <= 5)
+Console.WriteLine ("День недели - будни");
+else if (number >= 6 && number <= 7)
+Console.WriteLine ("День недели - выходной");
+else Console.WriteLine ("Неправильный ввод");
