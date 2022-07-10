dd82c159c47f1d975a27f693634928b9589f32ed
lubov <loveavduhova@gmail.com>
2022-07-10 21:47:46 +0700

    task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: Напишите программу, которая выводит случайное 
+трёхзначное число и удаляет вторую цифру этого числа. */
+
+Random rnd = new Random();
+int number = rnd.Next(100, 1000);
+int result = 0;
+Console.WriteLine ($"Число {number}");
+
+    result = number / 100 * 10 + number % 10;
+    Console.WriteLine($"Новое число: {result}");
